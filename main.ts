910dd00b5108c5d9511fafb3fbde783f3992fae1
ZinahHammadi <71913423+ZinahHammadi@users.noreply.github.com>
2020-10-04 17:31:25 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.showIcon(IconNames.SmallHeart)
+})
+basic.forever(function () {
+    dht11_dht22.queryData(
+    DHTtype.DHT11,
+    DigitalPin.P0,
+    true,
+    false,
+    true
+    )
+    if (dht11_dht22.readData(dataType.temperature) > 27) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.showIcon(IconNames.Yes)
+    } else {
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.showIcon(IconNames.No)
+    }
+})
